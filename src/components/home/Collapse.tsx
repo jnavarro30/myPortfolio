db2f9d765104db2a537d09cc4e34462242973b2f
Jesse Navarro <jnavarro30@live.com>
2022-05-09 02:49:36 -0700

    draft homepage layout

--- src/components/home/Collapse.tsx
+++ src/components/home/Collapse.tsx
@@ -1,23 +1,26 @@
-import { useState } from 'react';
-import { Collapse, ActionIcon, Title } from '@mantine/core';
-import { ChevronDown } from 'tabler-icons-react';
+import { useState } from "react";
+import { Collapse, ActionIcon, Title } from "@mantine/core";
+import { ChevronDown, ChevronUp } from "tabler-icons-react";
 
 function HomeCollapse() {
   const [opened, setOpen] = useState(false);
 
   return (
     <div className="flex flex-col items-center">
-        <Title order={1}>
-            About Me
-        </Title>
-        <ActionIcon onClick={() => setOpen((o) => !o)} size={40}>
-            <ChevronDown size={40}/>
-        </ActionIcon>
+      <Title order={1}>About Me</Title>
+      <ActionIcon onClick={() => setOpen((o) => !o)} size={40}>
+        {opened ? <ChevronUp size={40} /> : <ChevronDown size={40} />}
+      </ActionIcon>
 
-        <Collapse in={opened}>
-            {/* content... */}
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa commodi, sequi id tenetur debitis ab perferendis laudantium repellendus non quisquam accusamus impedit tempore necessitatibus quos doloremque atque? Dolorum commodi dolore quia amet, ipsum vero nulla qui, nobis placeat nam quos reprehenderit esse? Sunt aliquam optio, enim minima ad rerum veniam?
-        </Collapse>
+      <Collapse in={opened}>
+        {/* content... */}
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa commodi,
+        sequi id tenetur debitis ab perferendis laudantium repellendus non
+        quisquam accusamus impedit tempore necessitatibus quos doloremque atque?
+        Dolorum commodi dolore quia amet, ipsum vero nulla qui, nobis placeat
+        nam quos reprehenderit esse? Sunt aliquam optio, enim minima ad rerum
+        veniam?
+      </Collapse>
     </div>
   );
 }
