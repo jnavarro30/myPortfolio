d7c3fd723f92e64d9807a7648a34cbe5528076e7
Jesse Navarro <jnavarro30@live.com>
2022-05-14 03:06:48 -0700

    install and complete emailjs functionallity

--- src/components/contact/contact-form.tsx
+++ src/components/contact/contact-form.tsx
@@ -1,13 +1,56 @@
 import { TextInput, Textarea } from "@mantine/core";
+import emailjs from "emailjs-com";
 
 function ContactForm() {
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_1jqw7s3",
+        "template_eef2wpp",
+        e.target,
+        "GFj7TR0SGgWQfEuQc"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
-    <div className="grid gap-4">
-      <TextInput placeholder="first and last name" label="Full Name" required />
-      <TextInput placeholder="email" label="Email address" required />
-      <Textarea placeholder="comments" label="your comments" />
-      <button className="border-4 rounded w-1/2 p-2 justify-self-center hover:cursor-pointer">Submit</button>
-    </div>
+    <form className="grid gap-4" onSubmit={sendEmail}>
+      <TextInput
+        placeholder="first and last name"
+        label="Full Name"
+        name="name"
+        required
+      />
+      <TextInput
+        placeholder="email"
+        label="Email address"
+        name="email"
+        required
+      />
+      <TextInput
+        placeholder="subject"
+        label="Subject"
+        name="subject"
+        required
+      />
+      <Textarea placeholder="comments" label="your comments" name="comments" />
+      <button
+        className="border-4 rounded w-1/2 p-2 justify-self-center hover:cursor-pointer"
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
   );
 }
 
