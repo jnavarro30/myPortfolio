c648a43c0f32a4a1e88943ef52fc2379ab7d51f2
Jesse Navarro <jnavarro30@live.com>
2022-05-17 02:20:52 -0700

    project card layout with dummy text

--- src/components/portfolio/project-card.tsx
+++ src/components/portfolio/project-card.tsx
@@ -1,36 +1,67 @@
-import { Card, Image, Text, Badge, Button, Group, useMantineTheme } from '@mantine/core';
+import {
+  Card,
+  Image,
+  Text,
+  Badge,
+  Button,
+  Group,
+  useMantineTheme,
+} from "@mantine/core";
 
 interface ProjectProps {
-    projectImage: string
+  projectTitle: string;
+  projectDescription: string;
+  projectTechnologies: string[];
+  projectImage: string;
 }
 
-function ProjectCard({ projectImage }: ProjectProps) {
+function ProjectCard({
+  projectTitle,
+  projectDescription,
+  projectTechnologies,
+  projectImage,
+}: ProjectProps) {
   const theme = useMantineTheme();
 
-  const secondaryColor = theme.colorScheme === 'dark'
-    ? theme.colors.dark[1]
-    : theme.colors.gray[7];
+  const secondaryColor =
+    theme.colorScheme === "dark" ? theme.colors.dark[1] : theme.colors.gray[7];
+
+  const colors = ["violet", "indigo", "", "cyan"];
 
   return (
     <div className="w-5/6 m-auto">
       <Card shadow="sm" p="lg">
         <Card.Section>
-          <Image className ="mt-2" src={projectImage} height={160} alt="Norway" />
+          <Image
+            className="mt-2"
+            src={projectImage}
+            height={160}
+            alt="Norway"
+          />
         </Card.Section>
 
-        <Group position="apart" style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
-          <Text weight={500}>Kingdom of Dwarves</Text>
-          <Badge color="pink" variant="light">
-            Typescript
-          </Badge>
+        <Group
+          position="apart"
+          style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
+        >
+          <Text weight={500}>{projectTitle}</Text>
+          {projectTechnologies.map((tech, index) => (
+            <Badge color={colors[index]} variant="light" key={index}>
+              {tech}
+            </Badge>
+          ))}
         </Group>
 
         <Text size="sm" style={{ color: secondaryColor, lineHeight: 1.5 }}>
-          With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-          activities on and around the fjords of Norway
+          {projectDescription}
         </Text>
 
-        <Button variant="light" color="blue" fullWidth style={{ marginTop: 14 }}>
+        <Button
+          variant="light"
+          color="blue"
+          fullWidth
+          style={{ marginTop: 14 }}
+        >
           Demo
         </Button>
       </Card>
