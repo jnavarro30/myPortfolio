dda96467266236855632fb03aaeb59c57d1ffe55
Jesse Navarro <jnavarro30@live.com>
2022-05-20 01:26:02 -0700

    add scroll hook and add link tag to drawer list

--- src/components/portfolio/project-list.tsx
+++ src/components/portfolio/project-list.tsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
-import { List, Anchor, Box } from "@mantine/core";
+import { List, Box } from "@mantine/core";
 
 function ProjectList() {
   const [projectTitles, setProjectTitles] = useState([
     "Kingdom of Dwarves",
     "Pokemon Pokedex",
-    "Project 3",
-    "Project 4",
-    "Project 5",
-    "Project 6",
+    "Coming Soon",
+    "Coming Soon"
   ]);
 
   return (
@@ -16,27 +14,29 @@ function ProjectList() {
       {projectTitles.map((title, index) => {
         return (
           <List.Item key={index}>
-            <Box
-              sx={(theme) => ({
-                backgroundColor:
-                  theme.colorScheme === "dark"
-                    ? theme.colors.dark[6]
-                    : theme.colors.gray[0],
-                textAlign: "center",
-                padding: theme.spacing.xl,
-                borderRadius: theme.radius.md,
-                cursor: "pointer",
-
-                "&:hover": {
+            <a href={`#${index}`}>
+              <Box
+                sx={(theme) => ({
                   backgroundColor:
                     theme.colorScheme === "dark"
-                      ? theme.colors.dark[5]
-                      : theme.colors.gray[1],
-                },
-              })}
-            >
-              {title}
-            </Box>
+                      ? theme.colors.dark[6]
+                      : theme.colors.gray[0],
+                  textAlign: "center",
+                  padding: theme.spacing.xl,
+                  borderRadius: theme.radius.md,
+                  cursor: "pointer",
+
+                  "&:hover": {
+                    backgroundColor:
+                      theme.colorScheme === "dark"
+                        ? theme.colors.dark[5]
+                        : theme.colors.gray[1],
+                  },
+                })}
+              >
+                {title}
+              </Box>
+            </a>
           </List.Item>
         );
       })}
