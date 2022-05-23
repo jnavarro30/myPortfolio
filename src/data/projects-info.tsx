f67e805785f0192e10fe85d8c32dd12db991d72a
Jesse Navarro <jnavarro30@live.com>
2022-05-23 03:19:48 -0700

    add pokemon pokedex project

--- src/data/projects-info.tsx
+++ src/data/projects-info.tsx
@@ -1,37 +1,38 @@
 import KoD from "../images/kingdom_of_dwarves.png";
+import Pokedex from "../images/pokemon_pokedex.png";
 import comingSoon from "../images/coming_soon.jpg";
 
 export const PROJECTS = [
-    {
-      title: "Kingdom of Dwarves",
-      description:
-        "Kingdom of Dwarves is a cinematic-quality collection of 4,000 NFTs. Every dwarf is a unique, one-of-a-kind warrior created by Emmy and Academy Award-Winning artists with a portfolio of work including Game of Thrones, Westworld, and Gears of War.",
-      technologies: ["Typescript", "React", "Tailwind", "Next"],
-      image: KoD,
-      link: "https://app.kingdomofdwarves.io/",
-    },
-    {
-      title: "Coming Soon",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis perferendis nisi eum, facilis possimus adipisci, odit sint corporis quis eaque officia perspiciatis? Amet tenetur nemo quae repudiandae similique dicta dolor animi odit reiciendis culpa?",
-      technologies: ["Unkown", "Unkown", "Unkown", "Unkown"],
-      image: comingSoon,
-      link: "blank",
-    },
-    {
-      title: "Coming Soon",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis perferendis nisi eum, facilis possimus adipisci, odit sint corporis quis eaque officia perspiciatis? Amet tenetur nemo quae repudiandae similique dicta dolor animi odit reiciendis culpa?",
-      technologies: ["Unkown", "Unkown", "Unkown", "Unkown"],
-      image: comingSoon,
-      link: "blank",
-    },
-    {
-      title: "Coming Soon",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis perferendis nisi eum, facilis possimus adipisci, odit sint corporis quis eaque officia perspiciatis? Amet tenetur nemo quae repudiandae similique dicta dolor animi odit reiciendis culpa?",
-      technologies: ["Unkown", "Unkown", "Unkown", "Unkown"],
-      image: comingSoon,
-      link: "blank",
-    },
-  ];
+  {
+    title: "Kingdom of Dwarves",
+    description:
+      "Kingdom of Dwarves is a cinematic-quality collection of 4,000 NFTs. Every dwarf is a unique, one-of-a-kind warrior created by Emmy and Academy Award-Winning artists with a portfolio of work including Game of Thrones, Westworld, and Gears of War.",
+    technologies: ["Typescript", "React", "Tailwind", "Next"],
+    image: KoD,
+    link: "https://app.kingdomofdwarves.io/",
+  },
+  {
+    title: "Pokemon Pokedex",
+    description:
+      "Have you ever wanted to become a Pokemon trainer? Well, you are going to need your very own Pokedex to identify all those wild Pokemon! Simply use the controls already provided on the screen and bonus theme music included. ",
+    technologies: ["Javascript", "React", "Sass", "HTML"],
+    image: Pokedex,
+    link: "https://jn-pokedex.netlify.app/",
+  },
+  {
+    title: "Coming Soon",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis perferendis nisi eum, facilis possimus adipisci, odit sint corporis quis eaque officia perspiciatis? Amet tenetur nemo quae repudiandae similique dicta dolor animi odit reiciendis culpa?",
+    technologies: ["Unkown", "Unkown", "Unkown", "Unkown"],
+    image: comingSoon,
+    link: "blank",
+  },
+  {
+    title: "Coming Soon",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis perferendis nisi eum, facilis possimus adipisci, odit sint corporis quis eaque officia perspiciatis? Amet tenetur nemo quae repudiandae similique dicta dolor animi odit reiciendis culpa?",
+    technologies: ["Unkown", "Unkown", "Unkown", "Unkown"],
+    image: comingSoon,
+    link: "blank",
+  },
+];
