a5539f0c85909618f26a4fa89d0490773e85dd8f
Jesse Navarro <navarro@macbook.local>
2022-11-16 23:34:35 -0800

    initialize  hotjar

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { hotjar } from 'react-hotjar';
+
+hotjar.initialize('3216512', 6);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
